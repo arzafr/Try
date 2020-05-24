8edab27e0abd2a4ed5941f026937f863fcb3dbd8
arzafr <rezaarnanda080101@gmail.com>
2020-05-23 23:30:29 -0700

    major change

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-	int a, b, kali;
+	int a, b, kali, plus;
 	cout<<"Hello World"<<endl;
 
 	cout<<"I am Arzafr"<<endl;
@@ -16,6 +16,18 @@ int main(){
 
 	cout << a << " x "<< b <<" = "<< kali << endl;
 
+
+	cout <<"==================================================="<<endl;
+
+	cout <<"MAsukan 2 angka untuk di tambah kan(pisahkan Dengan spasi) : "<<endl;
+	cin >> a >> b;
+
+	plus = a + b;
+
+	cout << a << " + "<< b <<" = "<< plus << endl;
+
+
+	cin.get();
 	cin.get();
 	cin.get();
 	return 0;
