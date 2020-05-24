2c8c2ead54d95fde9288efc280fba814721e8f8b
arzafr <rezaarnanda080101@gmail.com>
2020-05-23 23:33:04 -0700

    major change

--- hello.cpp
+++ hello.cpp
@@ -9,25 +9,18 @@ int main(){
 
 	cout<<"I am Arzafr"<<endl;
 
-	cout <<"MAsukan 2 angka untuk di kali kan(pisahkan Dengan spasi) : "<<endl;
+	cout <<"MAsukan 2 angka untuk ditambah dan dikali kan(pisahkan Dengan spasi) : "<<endl;
 	cin >> a >> b;
 
 	kali = a * b;
 
 	cout << a << " x "<< b <<" = "<< kali << endl;
 
-
-	cout <<"==================================================="<<endl;
-
-	cout <<"MAsukan 2 angka untuk di tambah kan(pisahkan Dengan spasi) : "<<endl;
-	cin >> a >> b;
-
 	plus = a + b;
 
 	cout << a << " + "<< b <<" = "<< plus << endl;
 
 
-	cin.get();
 	cin.get();
 	cin.get();
 	return 0;
