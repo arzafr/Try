f9855691f63c90b54e5a1e1f0480eec01204a4c0
arzafr <rezaarnanda080101@gmail.com>
2020-05-23 23:09:15 -0700

    mengubah kode

--- hello.cpp
+++ hello.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 int main(){
 
+	int a, b, plus;
 	cout<<"Hello World"<<endl;
 
 	cout<<"I am Arzafr"<<endl;
 
+	cout <<"MAsukan 2 angka untuk di tmbahkan(pisahkan Dengan spasi) : "<<endl;
+	cin >> a >> b;
+
+	plus = a + b;
+
+	cout << a << " + "<< b <<" = "<< plus << endl;
+
+	cin.get();
 	cin.get();
 	return 0;
 }
