2e639ad5c1bc470c34da22fdbc747e3eec57d678
arzafr <rezaarnanda080101@gmail.com>
2020-05-26 02:10:29 +0700

    Menambah file nilai.cpp di branch mark

---
+++ nilai.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int nilai;
+    string matkul;
+    char indeks;
+
+    cout << "Masukan Nama Mata Kuliah : ";
+    cin >> matkul;
+    cout << "Masukan Nilai Anda (0 -100): ";
+    cin >> nilai;
+
+    if(nilai >= 0 && nilai <= 100 ){
+        if(nilai >= 80 ){
+            indeks = 'A';
+        } else if(nilai >= 65 && nilai < 80){
+            indeks = 'B';
+        } else if(nilai >= 55 && nilai < 65){
+            indeks = 'C';
+        } else if(nilai >= 40 && nilai < 55){
+            indeks = 'D';
+        } else {
+            indeks = 'E';
+        }
+    } else {
+        cout << "input nilai tidak valid"<<endl;
+    }
+
+    cout << "Dalam matkul "<< matkul <<" anda mendapat nilai "<< indeks << endl;
+    
+    cin.get();
+    cin.get();
+    
+    return 0;
+}
